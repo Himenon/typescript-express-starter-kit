6dccf28439fbe3ebd9f73bec0267d92ffb2329e3
K.Himeno <6715229+Himenon@users.noreply.github.com>
2023-04-19 21:02:55 +0900

    chore: add cors and sqlite3 (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+import cors from "cors";
 import express from "express";
+
+import * as Model from "./model";
 
 const SERVER_PORT = 9000;
 
+Model.createTable("hello");
+
 const createServer = (): express.Application => {
   const app = express();
+  app.use(cors());
 
   app.get("*", (req: express.Request, res: express.Response) => {
     res.json({ query: req.query });
