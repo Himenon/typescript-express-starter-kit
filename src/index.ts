abbb5021c3211c75570fad746c2c239758129022
K.Himeno <6715229+Himenon@users.noreply.github.com>
2023-04-19 21:00:55 +0900

    chore: add cors and sqlite3

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import cors from "cors";
 import express from "express";
 
 const SERVER_PORT = 9000;
 
 const createServer = (): express.Application => {
   const app = express();
+  app.use(cors());
 
   app.get("*", (req: express.Request, res: express.Response) => {
     res.json({ query: req.query });
---
+++ src/model.ts
@@ -0,0 +1,12 @@
+import Database from "better-sqlite3";
+
+const db = new Database(":memory:");
+
+export const createTable = (tableName: string) => {
+  db.exec(`create table if not exists ${tableName} ( json JSON NOT NULL );`);
+}
+
+export const insert = (tableName: string, data: string) => {
+  const statement = db.prepare(`INSERT INTO ${tableName} (json) VALUES (?)`);
+  return statement.run(data);
+};
